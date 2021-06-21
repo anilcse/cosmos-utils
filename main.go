d247d6f0f2b6e8c05e2e592fac732cfb5eb15a38
PrathyushaLakkireddy <prathyusha@vitwit.com>
2021-06-21 17:35:57 +0530

    Removed commented code

--- main.go
+++ main.go
@@ -50,33 +50,4 @@ func main() {
 	}()
 
 	wg.Wait()
-
-	// m := targets.InitTargets(cfg)
-	// runner := targets.NewRunner()
-
-	// c, err := client.NewHTTPClient(client.HTTPConfig{
-	// 	Addr:     fmt.Sprintf("http://localhost:%s", cfg.InfluxDB.Port),
-	// 	Username: cfg.InfluxDB.Username,
-	// 	Password: cfg.InfluxDB.Password,
-	// })
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer c.Close()
-
-	// var wg sync.WaitGroup
-	// for _, tg := range m.List {
-	// 	wg.Add(1)
-	// 	go func(target targets.Target) {
-	// 		scrapeRate, err := time.ParseDuration(target.ScraperRate)
-	// 		if err != nil {
-	// 			log.Fatal(err)
-	// 		}
-	// 		for {
-	// 			runner.Run(target.Func, target.HTTPOptions, cfg, c)
-	// 			time.Sleep(scrapeRate)
-	// 		}
-	// 	}(tg)
-	// }
-	// wg.Wait()
 }
