ac68963163fddee92b6129aa76cee69c55d7d7f4
PrathyushaLakkireddy <prathyusha@vitwit.com>
2021-09-14 12:21:31 +0530

    Add consition in shell script

--- distribution_script.sh
+++ distribution_script.sh
@@ -3,8 +3,17 @@
 echo "--------- Run withdraw-rewards command-----------"
 
 tx=$(regen tx bank send test1 regen1jmwm6xxwgzwmsfmwjz7eqfmqtupfks8jd40npy 1000uregen --fees 10uregen --chain-id test -y)
+#echo "${tx}"
 code=$(echo "${tx}"| jq -r '.code')
+txHash=$(echo "${tx}" | jq -r '.txhash')
 echo $code
+echo $txHash
+if [ "$code" -eq 0 ];
+then
+    echo "Send tx is successfull!!  txHash is : $txHash"
+else 
+    echo "Send tx is failed!!!!   txHash is : $txHash"
+fi
 
 
 
