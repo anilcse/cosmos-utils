365027e4946ffeff46afeaf427f29798b11cd457
PrathyushaLakkireddy <prathyusha@vitwit.com>
2021-09-14 13:05:08 +0530

    Add withdraw-rewards

--- distribution_script.sh
+++ distribution_script.sh
@@ -1,8 +1,25 @@
 #/bin/sh
 
-echo "--------- Run withdraw-rewards command-----------"
+printf "Exported values::\n Daemon : $DAEMON\n Key : $KEY\n ChainID : $CHAINID\n Node : $NODE\n FEE :$FEE\n ValidatorAddress : $VALADDRESS\n"
 
-tx=$(regen tx bank send test1 regen1jmwm6xxwgzwmsfmwjz7eqfmqtupfks8jd40npy 1000uregen --fees 10uregen --chain-id test -y)
+echo "--------- Running withdraw-rewards command-----------"
+
+wrTx=$("${DAEMON}" tx distribution withdraw-rewards "${VALADDRESS}" --from $KEY --fees "${FEE}" --chain-id "${CHAINID}" --node "${NODE}" -y)
+#echo $wrTx
+wrCode=$(echo "${wrTx}"| jq -r '.code')
+wrtxHash=$(echo "${wrTx}" | jq '.txhash')
+echo $wrCode
+echo $wrtxHash
+if [ "$wrCode" -eq 0 ];
+then
+    echo "withdraw-rewards tx is successfull!!  txHash is : $wrtxHash"
+else 
+    echo "withdraw-rewards tx is failed!!!!   txHash is : $wrtxHash"
+fi
+
+echo "------ Running withdraw-all-rewards tx --------"
+
+tx=$($DAEMON tx distribution withdraw-all-rewards --from $KEY --fees $FEE --chain-id $CHAINID --node $NODE -y)
 #echo "${tx}"
 code=$(echo "${tx}"| jq -r '.code')
 txHash=$(echo "${tx}" | jq -r '.txhash')
@@ -10,12 +27,13 @@ echo $code
 echo $txHash
 if [ "$code" -eq 0 ];
 then
-    echo "Send tx is successfull!!  txHash is : $txHash"
+    echo "withdraw-all-rewards tx is successfull!!  txHash is : $txHash"
 else 
-    echo "Send tx is failed!!!!   txHash is : $txHash"
+    echo "withdraw-all-rewards tx is failed!!!!   txHash is : $txHash"
 fi
 
 
 
 #$DAEMON tx distribution withdraw-rewards --from $KEY --fees $FEE --chain-id $CHAINID --keyring-backend test --node $NODE -y
 #sleep 10
+#tx=$("${DAEMON}" tx bank send test1 regen1jmwm6xxwgzwmsfmwjz7eqfmqtupfks8jd40npy 1000uregen --fees 10uregen --chain-id "${CHAINID}" -y)
