cd58006b9a28258e0fdf28be15f86fb9fbd78d05
PrathyushaLakkireddy <prathyusha@vitwit.com>
2021-09-15 12:29:42 +0530

    Add redelegation tx in script

--- scripts/staking_script.sh
+++ scripts/staking_script.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-printf "Exported values::\n Daemon : $DAEMON\n Key : $KEY\n ChainID : $CHAINID\n Node : $NODE\n FEE :$FEE\n ValidatorAddress : $VALADDRESS\n Amount : $AMOUNT\n"
+printf "Exported values::\n Daemon : $DAEMON\n Key : $KEY\n ChainID : $CHAINID\n Node : $NODE\n FEE :$FEE\n ValidatorAddress : $VALADDRESS\n DestinationValidatorAddress: $DSTVALADDR\n Amount : $AMOUNT\n"
 
 echo "--------- Running delegate tx command-----------"
 
@@ -14,6 +14,22 @@ then
     echo "**** Delegate tx is SUCCESSFULL!!  txHash is : $dtxHash ****"
 else 
     echo "**** Delegate tx is FAILED!!!!   txHash is : $dtxHash  ***"
+fi
+
+echo
+
+echo "--------- Running redelegate tx command-----------"
+
+rdTx=$("${DAEMON}" tx staking redelegate "${VALADDRESS}" "${DSTVALADDR}" "${AMOUNT}" --from $KEY --fees "${FEE}" --chain-id "${CHAINID}" --node "${NODE}" -y)
+rdTxCode=$(echo "${rdTx}"| jq -r '.code')
+rdtxHash=$(echo "${rdTx}" | jq '.txhash')
+echo "Code is : $rdTxCode"
+echo
+if [ "$rdTxCode" -eq 0 ];
+then
+    echo "**** Delegate tx is SUCCESSFULL!!  txHash is : $rdtxHash ****"
+else 
+    echo "**** Delegate tx is FAILED!!!!   txHash is : $rdtxHash  ***"
 fi
 
 echo
