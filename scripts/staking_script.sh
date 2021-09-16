5c1b9462995ad3dba2943a1d289f269dd0bbb558
PrathyushaLakkireddy <prathyusha@vitwit.com>
2021-09-16 15:48:01 +0530

    Update staking script to get val address

--- scripts/staking_script.sh
+++ scripts/staking_script.sh
@@ -1,14 +1,50 @@
 #/bin/sh
 
-
 display_usage() {
-    printf "** Please check the exported values:: **\n Daemon : $DAEMON\n Key : $KEY\n ChainID : $CHAINID\n Node : $NODE\n FEE :$FEE\n ValidatorAddress : $VALADDRESS\n DestinationValidatorAddress: $DSTVALADDR\n Amount : $AMOUNT\n"
+    printf "** Please check the exported values:: **\n Daemon : $DAEMON\n Key : $KEY\n ChainID : $CHAINID\n Node : $NODE\n FEE :$FEE\n Amount : $AMOUNT\n"
     exit 1
 }
 
-if [ -z $DAEMON ] || [ -z $KEY ] || [ -z $CHAINID ] || [ -z $NODE ] || [ -z $FEE ] || [ -z $VALADDRESS ] || [ -z $DSTVALADDR] || [ -z $AMOUNT]
+if [ -z $DAEMON ] || [ -z $KEY ] || [ -z $CHAINID ] || [ -z $NODE ] || [ -z $FEE ] || [ -z $AMOUNT]
 then 
     display_usage
+fi
+
+echo
+
+echo "--------- Get validator address -----------"
+va1=$("${DAEMON}" keys show validator1 --bech val --keyring-backend test --output json)
+v1=$(echo "${va1}" | jq -r '.address')
+echo "** validator1 address :: $v1 **"
+
+va2=$("${DAEMON}" keys show validator2 --bech val --keyring-backend test --output json)
+v2=$(echo "${va2}" | jq -r '.address')
+echo "** validator2 address :: $v2 **"
+
+va3=$("${DAEMON}" keys show validator3 --bech val --keyring-backend test --output json)
+v3=$(echo "${va3}" | jq -r '.address')
+echo "** validator3 address :: $v3 **"
+
+va4=$("${DAEMON}" keys show validator4 --bech val --keyring-backend test --output json)
+v4=$(echo "${va4}" | jq -r '.address')
+echo "** validator4 address :: $v4 **"
+
+echo
+echo "--------- Running delegate tx from validator1 to validator2-----------"
+
+regen keys show validator --bech val --keyring-backend test //check
+
+
+dTx=$("${DAEMON}" tx staking delegate "${VALADDRESS}" "${AMOUNT}" --from validator1 --fees "${FEE}" --chain-id "${CHAINID}" --keyring-backend test --node "${NODE}" -y)
+dTxCode=$(echo "${dTx}"| jq -r '.code')
+dtxHash=$(echo "${dTx}" | jq '.txhash')
+echo "Code is : $dTxCode"
+echo
+if [ "$dTxCode" -eq 0 ];
+then
+    echo "**** Delegate tx is SUCCESSFULL!!  txHash is : $dtxHash ****"
+else 
+    echo "**** Delegate tx is FAILED!!!!   txHash is : $dtxHash and REASON : $(echo "${dTx}" | jq '.raw_log')***"
 fi
 
 echo
@@ -47,7 +83,7 @@ echo
 echo "--------- Running unbond tx command-----------"
 
 ubTx=$("${DAEMON}" tx staking unbond "${VALADDRESS}" "${AMOUNT}" --from $KEY --fees "${FEE}" --chain-id "${CHAINID}" --keyring-backend test --node "${NODE}" -y)
-#echo $ubTx
+#echo $ubTx 1000 or 10000stake to be replaced in amount
 ubTxCode=$(echo "${ubTx}"| jq -r '.code')
 ubtxHash=$(echo "${ubTx}" | jq '.txhash')
 echo "Code is : $ubTxCode"
