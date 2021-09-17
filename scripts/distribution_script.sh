37067f966412a1465e98a6e7d23c85da9aa80ebe
PrathyushaLakkireddy <prathyusha@vitwit.com>
2021-09-17 11:44:39 +0530

    Update withdraw rewards commssion tx

--- scripts/distribution_script.sh
+++ scripts/distribution_script.sh
@@ -60,42 +60,48 @@ do
     echo "Code is : $wrCode"
     if [ "$wrCode" -eq 0 ];
     then
-        echo "**** withdraw-rewards of $VALADDRESS and key $FROMKEY is successfull!!  txHash is : $wrtxHash ****"
+        echo "**** withdraw-rewards of ( $VALADDRESS and key $FROMKEY ) is successfull!!  txHash is : $wrtxHash ****"
     else 
-        echo "**** withdraw-rewards of $VALADDRESS and key $FROMKEY is failed!!!!   txHash is : $wrtxHash and REASON : wrtxHash=$(echo "${wrTx}" | jq '.raw_log') ****"
+        echo "**** withdraw-rewards of ( $VALADDRESS and key $FROMKEY ) is failed!!!!   txHash is : $wrtxHash and REASON : wrtxHash=$(echo "${wrTx}" | jq '.raw_log') ****"
     fi
 done
 echo
 
-echo "--------- Running withdraw-rewards tx-----------"
+#Start of for loop to run delegation txs
+for a in 1 2 3 4
+do
+    if [ $a == 1 ]
+    then
+        FROMKEY="validator1"
+        VALADDRESS=$v1
+    elif [ $a == 2 ]
+    then
+        FROMKEY="validator2"
+        VALADDRESS=$v2
+    elif [ $a == 3 ]
+    then
+        FROMKEY="validator3"
+        VALADDRESS=$v3
+    else [ $a == 4 ]
+        FROMKEY="validator4"
+        VALADDRESS=$v4
+    fi
+    # Print the value
+    echo "Iteration no $a and values of address : $VALADDRESS and key : $FROMKEY"
+    echo "--------- withdraw-rewards commission of $FROMKEY-----------"
 
-wrTx=$("${DAEMON}" tx distribution withdraw-rewards "${VALADDRESS}" --from $KEY --fees "${FEE}" --chain-id "${CHAINID}" --keyring-backend test --node "${NODE}" -y)
-wrCode=$(echo "${wrTx}"| jq -r '.code')
-wrtxHash=$(echo "${wrTx}" | jq '.txhash')
-echo "Code is : $wrCode"
-if [ "$wrCode" -eq 0 ];
-then
-    echo "**** withdraw-rewards tx is successfull!!  txHash is : $wrtxHash ****"
-else 
-    echo "**** withdraw-rewards tx is failed!!!!   txHash is : $wrtxHash and REASON : wrtxHash=$(echo "${wrTx}" | jq '.raw_log') ****"
-fi
-
-echo
-
-echo "--------- Running withdraw-rewards commission tx-----------"
-
-wrcTx=$("${DAEMON}" tx distribution withdraw-rewards "${VALADDRESS}" --from $KEY --commission --fees "${FEE}" --chain-id "${CHAINID}" --keyring-backend test --node "${NODE}" -y)
-#echo $wrTx
-wrcCode=$(echo "${wrcTx}"| jq -r '.code')
-wrctxHash=$(echo "${wrcTx}" | jq '.txhash')
-echo "Code is : $wrcCode"
-if [ "$wrcCode" -eq 0 ];
-then
-    echo "**** withdraw-rewards commission tx is successfull!!  txHash is : $wrctxHash ****"
-else 
-    echo "**** withdraw-rewards comission tx is failed!!!!   txHash is : $wrctxHash and REASON : $(echo "${wrcTx}" | jq '.raw_log') ****"
-fi
-
+    wrcTx=$("${DAEMON}" tx distribution withdraw-rewards "${VALADDRESS}" --from $FROMKEY --commission --fees 1000"${DENOM}" --chain-id "${CHAINID}" --keyring-backend test --node "${NODE}" -y)
+    #echo $wrTx
+    wrcCode=$(echo "${wrcTx}"| jq -r '.code')
+    wrctxHash=$(echo "${wrcTx}" | jq '.txhash')
+    echo "Code is : $wrcCode"
+    if [ "$wrcCode" -eq 0 ];
+    then
+        echo "**** withdraw-rewards commission of ( $VALADDRESS and key $FROMKEY ) is successfull!!  txHash is : $wrctxHash ****"
+    else 
+        echo "**** withdraw-rewards comission of ( $VALADDRESS and key $FROMKEY ) is failed!!!!   txHash is : $wrctxHash and REASON : $(echo "${wrcTx}" | jq '.raw_log') ****"
+    fi
+done
 echo
 
 echo "------ Running withdraw-all-rewards tx --------"
