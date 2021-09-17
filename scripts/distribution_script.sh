26be610befc210f55cb0f861db99a0c4e76dbcbd
PrathyushaLakkireddy <prathyusha@vitwit.com>
2021-09-17 11:31:05 +0530

    Update withdraw rewards tx

--- scripts/distribution_script.sh
+++ scripts/distribution_script.sh
@@ -2,7 +2,7 @@
 
 display_usage() {
     printf "** Please check the exported values:: **\n Daemon : $DAEMON\n Key : $KEY\n ChainID : $CHAINID\n Node : $NODE\n FEE : $FEE\n ValidatorAddress : $VALADDRESS\n"
-    exit 1
+    # exit 1
 }
 
 if [ -z $DAEMON ] || [ -z $KEY ] || [ -z $CHAINID ] || [ -z $NODE ] || [ -z $FEE ] || [ -z $VALADDRESS ]
@@ -11,6 +11,62 @@ then
 fi
 
 echo
+
+echo "--------- Get validator addresses -----------"
+va1=$("${DAEMON}" keys show validator1 --bech val --keyring-backend test --output json)
+v1=$(echo "${va1}" | jq -r '.address')
+echo "** validator1 address :: $v1 **"
+
+va2=$("${DAEMON}" keys show validator2 --bech val --keyring-backend test --output json)
+v2=$(echo "${va2}" | jq -r '.address')
+echo "** validator2 address :: $v2 **"
+
+va3=$("${DAEMON}" keys show validator3 --bech val --keyring-backend test --output json)
+v3=$(echo "${va3}" | jq -r '.address')
+echo "** validator3 address :: $v3 **"
+
+va4=$("${DAEMON}" keys show validator4 --bech val --keyring-backend test --output json)
+v4=$(echo "${va4}" | jq -r '.address')
+echo "** validator4 address :: $v4 **"
+
+echo
+
+#Start of for loop to run delegation txs
+for a in 1 2 3 4
+do
+    if [ $a == 1 ]
+    then
+        FROMKEY="validator1"
+        VALADDRESS=$v1
+    elif [ $a == 2 ]
+    then
+        FROMKEY="validator2"
+        VALADDRESS=$v2
+    elif [ $a == 3 ]
+    then
+        FROMKEY="validator3"
+        VALADDRESS=$v3
+    else [ $a == 4 ]
+        FROMKEY="validator4"
+        VALADDRESS=$v4
+    fi
+    # Print the value
+    echo "Iteration no $a and values of address : $VALADDRESS and key : $FROMKEY"
+    echo "--------- withdraw-rewards of $FROMKEY-----------"
+
+    wrTx=$("${DAEMON}" tx distribution withdraw-rewards "${VALADDRESS}" --from $FROMKEY --fees 1000"${DENOM}" --chain-id "${CHAINID}" --keyring-backend test --node "${NODE}" -y)
+    wrCode=$(echo "${wrTx}"| jq -r '.code')
+    wrtxHash=$(echo "${wrTx}" | jq '.txhash')
+    echo "Code is : $wrCode"
+    if [ "$wrCode" -eq 0 ];
+    then
+        echo "**** withdraw-rewards of $VALADDRESS and key $FROMKEY is successfull!!  txHash is : $wrtxHash ****"
+    else 
+        echo "**** withdraw-rewards of $VALADDRESS and key $FROMKEY is failed!!!!   txHash is : $wrtxHash and REASON : wrtxHash=$(echo "${wrTx}" | jq '.raw_log') ****"
+    fi
+done
+echo
+
 echo "--------- Running withdraw-rewards tx-----------"
 
 wrTx=$("${DAEMON}" tx distribution withdraw-rewards "${VALADDRESS}" --from $KEY --fees "${FEE}" --chain-id "${CHAINID}" --keyring-backend test --node "${NODE}" -y)
