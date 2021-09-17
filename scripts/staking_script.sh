f7382bb32a0bebe52c13efe3fe69f69b8dbfac6f
PrathyushaLakkireddy <prathyusha@vitwit.com>
2021-09-17 12:45:17 +0530

    update variables in staking script

--- scripts/staking_script.sh
+++ scripts/staking_script.sh
@@ -36,33 +36,33 @@ for a in 1 2 3
 do
     if [ $a == 1 ]
     then
-        fromKey="validator1"
-        to=$v2
-        toKey="validator2"
+        FROMKEY="validator1"
+        TO=$v2
+        TOKEY="validator2"
     elif [ $a == 2 ]
     then
-        fromKey="validator2"
-        to=$v3
-        toKey="validator3"
+        FROMKEY="validator2"
+        TO=$v3
+        TOKEY="validator3"
     else [ $a == 3 ]
-        fromKey="validator3"
-        to=$v4
-        toKey="validator4"
+        FROMKEY="validator3"
+        TO=$v4
+        TOKEY="validator4"
     fi
     # Print the value
-    echo "Iteration no $a and values of from : $fromKey to : $to"
-    echo "--------- Delegation from $from to $to-----------"
+    echo "Iteration no $a and values of from : $FROMKEY to : $TO"
+    echo "--------- Delegation from $FROMKEY to $TO-----------"
 
-    dTx=$("${DAEMON}" tx staking delegate "${to}" 10000"${DENOM}" --from $fromKey --fees 1000"${DENOM}" --chain-id "${CHAINID}" --keyring-backend test --node "${NODE}" -y)
+    dTx=$("${DAEMON}" tx staking delegate "${TO}" 10000"${DENOM}" --from $FROMKEY --fees 1000"${DENOM}" --chain-id "${CHAINID}" --keyring-backend test --node "${NODE}" -y)
     dTxCode=$(echo "${dTx}"| jq -r '.code')
     dtxHash=$(echo "${dTx}" | jq '.txhash')
     echo "Code is : $dTxCode"
     echo
     if [ "$dTxCode" -eq 0 ];
     then
-        echo "**** Delegation from $fromKey to $toKey is SUCCESSFULL!!  txHash is : $dtxHash ****"
+        echo "**** Delegation from $FROMKEY to $TOKEY is SUCCESSFULL!!  txHash is : $dtxHash ****"
     else 
-        echo "**** Delegation from $fromKey to $toKey has FAILED!!!!   txHash is : $dtxHash and REASON : $(echo "${dTx}" | jq '.raw_log')***"
+        echo "**** Delegation from $FROMKEY to $TOKEY has FAILED!!!!   txHash is : $dtxHash and REASON : $(echo "${dTx}" | jq '.raw_log')***"
     fi
     echo
 done
@@ -74,42 +74,42 @@ for a in 1 2 3 4
 do
     if [ $a == 1 ]
     then
-        from=$v4
-        to=$v3
-        fromKey="validator4"
-        toKey="validator3"
+        FROM=$v4
+        TO=$v3
+        FROMKEY="validator4"
+        TOKEY="validator3"
     elif [ $a == 2 ]
     then
-        from=$v3
-        to=$v2
-        fromKey="validator3"
-        toKey="validator2"
+        FROM=$v3
+        TO=$v2
+        FROMKEY="validator3"
+        TOKEY="validator2"
     elif [ $a == 3 ]
     then
-        from=$v2
-        to=$v1
-        fromKey="validator2"
-        toKey="validator1"
+        FROM=$v2
+        TO=$v1
+        FROMKEY="validator2"
+        TOKEY="validator1"
     else [$a == 4]
-        from=$v4
-        to=$v3
-        fromKey="validator4"
-        toKey="validator3"
+        FROM=$v4
+        TO=$v3
+        FROMKEY="validator4"
+        TOKEY="validator3"
     fi
     # Print the value
-    echo "Iteration no $a and values of from : $from to : $to"
-    echo "--------- Redelegation from $from to $to-----------"
+    echo "Iteration no $a and values of from : $FROM to : $TO"
+    echo "--------- Redelegation from $FROM to $TO-----------"
 
-    rdTx=$("${DAEMON}" tx staking redelegate "${from}" "${to}" 10000"${DENOM}" --from $fromKey --fees 1000"${DENOM}" --chain-id "${CHAINID}" --keyring-backend test --node "${NODE}" -y)
+    rdTx=$("${DAEMON}" tx staking redelegate "${FROM}" "${TO}" 10000"${DENOM}" --from "${FROMKEY}" --fees 1000"${DENOM}" --chain-id "${CHAINID}" --keyring-backend test --node "${NODE}" -y)
     rdTxCode=$(echo "${rdTx}"| jq -r '.code')
     rdtxHash=$(echo "${rdTx}" | jq '.txhash')
     echo "Code is : $rdTxCode"
     echo
     if [ "$rdTxCode" -eq 0 ];
     then
-        echo "**** Redelegation from $fromKey to $toKey is SUCCESSFULL!!  txHash is : $rdtxHash ****"
+        echo "**** Redelegation from $FROMKEY to $TOKEY is SUCCESSFULL!!  txHash is : $rdtxHash ****"
     else 
-        echo "**** Redelegation from $fromKey to $toKey has FAILED!!!!   txHash is : $rdtxHash and REASON : $(echo "${rdTx}" | jq '.raw_log') ***"
+        echo "**** Redelegation from $FROMKEY to $TOKEY has FAILED!!!!   txHash is : $rdtxHash and REASON : $(echo "${rdTx}" | jq '.raw_log') ***"
     fi
     echo
 done
@@ -120,34 +120,34 @@ for a in 1 2 3 4
 do
     if [ $a == 1 ]
     then
-        from=$v1
-        fromKey="validator1"
+        FROM=$v1
+        FROMKEY="validator1"
     elif [ $a == 2 ]
     then
-        from=$v2
-        fromKey="validator2"
+        FROM=$v2
+        FROMKEY="validator2"
     elif [ $a == 3 ]
     then
-        from=$v3
-        fromKey="validator3"
+        FROM=$v3
+        FROMKEY="validator3"
     else
-        from=$v4
-        fromKey="validator4"
+        FROM=$v4
+        FROMKEY="validator4"
     fi
     # Print the value
-    echo "Iteration no $a and values of from : $from and fromKey : $fromKey"
-    echo "--------- Running unbond tx command-----------"
+    echo "Iteration no $a and values of from : $FROM and fromKey : $FROMKEY"
+    echo "--------- Running unbond tx command------------"
 
-    ubTx=$("${DAEMON}" tx staking unbond "${from}" 10000"${DENOM}" --from $fromKey --fees 1000"${DENOM}" --chain-id "${CHAINID}" --keyring-backend test --node "${NODE}" -y)
+    ubTx=$("${DAEMON}" tx staking unbond "${FROM}" 10000"${DENOM}" --from "${FROMKEY}" --fees 1000"${DENOM}" --chain-id "${CHAINID}" --keyring-backend test --node "${NODE}" -y)
     ubTxCode=$(echo "${ubTx}"| jq -r '.code')
     ubtxHash=$(echo "${ubTx}" | jq '.txhash')
     echo "Code is : $ubTxCode"
     echo
     if [ "$ubTxCode" -eq 0 ];
     then
-        echo "**** Unbond tx of $fromKey is SUCCESSFULL!!  txHash is : $ubtxHash ****"
+        echo "**** Unbond tx ( of $FROM and key $FROMKEY ) is SUCCESSFULL!!  txHash is : $ubtxHash ****"
     else 
-        echo "**** Unbond tx of $fromKey if FAILED!!!!   txHash is : $ubtxHash  and REASON : $(echo "${ubTx}" | jq '.raw_log')  ***"
+        echo "**** Unbond tx ( of $FROM and key $FROMKEY ) if FAILED!!!!   txHash is : $ubtxHash  and REASON : $(echo "${ubTx}" | jq '.raw_log')  ***"
     fi
     echo
 done
