c78d10e348580c1a95fc457ab9d4b41678cef02d
PrathyushaLakkireddy <prathyusha@vitwit.com>
2021-09-20 14:19:11 +0530

    fix issue of jq command

--- scripts/proposal_vote.sh
+++ scripts/proposal_vote.sh
@@ -26,12 +26,51 @@ echo
 
 echo "--------Get voting period proposals--------------"
 vp=$("${DAEMON}" q gov proposals --status voting_period --output json)
-len=$(echo "${vp}" | jq '.proposals | length' )
+len=$(echo "${vp}" | jq -r '.proposals | length' )
 echo "** Length of voting period proposals : $len **"
 
-for i in $(seq 1 $len);
+for row in $(echo "${vp}" | jq -r '.proposals | .[] | @base64'); do
+  # _jq() {
+  #   echo ${row} | base64 --decode | jq -r ${1}
+  # }
+  echo ${row} | base64 --decode | jq -r '.proposal_id'
+done
+
+arr=$(echo "${vp}" | jq -r '.proposals | map(.proposal_id)')
+echo $arr
+for i in ${arr[@]}; do
+  echo "Value.....$i"
+done
+#echo ${arr[0]}
+#echo ${arr[@]:1:2}
+#echo ${arr[@]:0:1}
+
+echo "Print all : ${arr[@]}   and ${arr[0]}"
+# iterate through the Bash array
+#for item in "${arr[@]}"; do
+   #echo $item
+   #echo "In loop ${arr[@]:item-1:item}"
+  # do your stuff
+#done
+
+echo "ele ${arr[@]:0:1}"
+echo "ele ${arr[@]:1:2}"
+echo "ele ${arr[@]:2:3}"
+echo "ele ${arr[@]:3:4}"
+
+tl=`expr $len + $len - 1`
+echo "total len: $s"
+
+for i in $(seq 1 $tl);
 do
-    echo $i
-    echo $(echo "${vp}" | jq -r '.proposals[] | .proposal_id')
+    j=`expr $i - 1`
+    PID=${arr[@]:j:i}
+    echo "Proposal id i ${i} and j ${j} ${PID}" #print porposal ids
+
+    echo "${i} and ${j}  ${arr[@]:2:3} "
+
+
+    #echo "In loop ${arr[@]:item-1:item}"
+   # echo $(echo "${vp}" | jq -c '.proposals[] | .proposal_id')
 done
 
