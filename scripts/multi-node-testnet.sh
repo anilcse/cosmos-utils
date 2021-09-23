4d8849425c8453f5b6da572faf2b76671b020f7b
PrathyushaLakkireddy <prathyusha@vitwit.com>
2021-09-23 11:36:02 +0530

    update config addresses

--- scripts/multi-node-testnet.sh
+++ scripts/multi-node-testnet.sh
@@ -93,7 +93,7 @@ echo "----------Update chain config---------"
 echo "----------Updating $DAEMON_HOME_1 chain config-----------"
 
 sed -i 's#tcp://127.0.0.1:26657#tcp://0.0.0.0:16657#g' $DAEMON_HOME_1/config/config.toml
-sed -i 's#tcp://127.0.0.1:26656#tcp://0.0.0.0:16656#g' $DAEMON_HOME_1/config/config.toml
+sed -i 's#tcp://0.0.0.1:26656#tcp://0.0.0.0:16656#g' $DAEMON_HOME_1/config/config.toml
 sed -i 's/"timeout_commit" = "5s"' $DAEMON_HOME_1/config/config.toml
 sed -i 's#0.0.0.0:9090#0.0.0.0:1090#g' $DAEMON_HOME_1/config/app.toml
 sed -i 's#0.0.0.0:9091#0.0.0.0:1091#g' $DAEMON_HOME_1/config/app.toml
@@ -104,7 +104,7 @@ sed -i "s/stake/$DENOM/g" $DAEMON_HOME_1/config/genesis.json
 echo "----------Updating $DAEMON_HOME_2 chain config-----------"
 
 sed -i 's#tcp://127.0.0.1:26657#tcp://0.0.0.0:26657#g' $DAEMON_HOME_2/config/config.toml
-sed -i 's#tcp://127.0.0.1:26656#tcp://0.0.0.0:26656#g' $DAEMON_HOME_2/config/config.toml
+sed -i 's#tcp://0.0.0.1:26656#tcp://0.0.0.0:26656#g' $DAEMON_HOME_2/config/config.toml
 sed -i 's/"timeout_commit" = "5s"' $DAEMON_HOME_2/config/config.toml
 sed -i 's#0.0.0.0:9090#0.0.0.0:2090#g' $DAEMON_HOME_2/config/app.toml
 sed -i 's#0.0.0.0:9091#0.0.0.0:2091#g' $DAEMON_HOME_2/config/app.toml
@@ -112,7 +112,7 @@ sed -i 's#0.0.0.0:9091#0.0.0.0:2091#g' $DAEMON_HOME_2/config/app.toml
 echo "----------Updating $DAEMON_HOME_3 chain config------------"
 
 sed -i 's#tcp://127.0.0.1:26657#tcp://0.0.0.0:36657#g' $DAEMON_HOME_3/config/config.toml
-sed -i 's#tcp://127.0.0.1:26656#tcp://0.0.0.0:36656#g' $DAEMON_HOME_3/config/config.toml
+sed -i 's#tcp://0.0.0.1:26656#tcp://0.0.0.0:36656#g' $DAEMON_HOME_3/config/config.toml
 sed -i 's/"timeout_commit" = "5s"' $DAEMON_HOME_3/config/config.toml
 sed -i 's#0.0.0.0:9090#0.0.0.0:3090#g' $DAEMON_HOME_3/config/app.toml
 sed -i 's#0.0.0.0:9091#0.0.0.0:3091#g' $DAEMON_HOME_3/config/app.toml
@@ -120,7 +120,7 @@ sed -i 's#0.0.0.0:9091#0.0.0.0:3091#g' $DAEMON_HOME_3/config/app.toml
 echo "----------Updating $DAEMON_HOME_4 chain config------------"
 
 sed -i 's#tcp://127.0.0.1:26657#tcp://0.0.0.0:46657#g' $DAEMON_HOME_4/config/config.toml
-sed -i 's#tcp://127.0.0.1:26656#tcp://0.0.0.0:46656#g' $DAEMON_HOME_4/config/config.toml
+sed -i 's#tcp://0.0.0.0:26656#tcp://0.0.0.0:46656#g' $DAEMON_HOME_4/config/config.toml
 sed -i 's/"timeout_commit" = "5s"' $DAEMON_HOME_4/config/config.toml
 sed -i 's#0.0.0.0:9090#0.0.0.0:4090#g' $DAEMON_HOME_4/config/app.toml
 sed -i 's#0.0.0.0:9091#0.0.0.0:4091#g' $DAEMON_HOME_4/config/app.toml
