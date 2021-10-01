3fdc87ccf1baf4f5b6c44e18eae22ee26cf9c26f
PrathyushaLakkireddy <prathyusha@vitwit.com>
2021-10-01 16:25:09 +0530

    added shutdown script

---
+++ scripts/shutdown_nodes.sh
@@ -0,0 +1,42 @@
+#/bin/sh
+
+NODES=$1
+if [ -z $NODES ]
+then
+    NODES=2
+fi
+
+echo "**** Number of nodes to be shutdown: $NODES ****"
+
+echo "------- Running unsafe reset all ---------"
+
+for (( a=1; a<=$NODES; a++ ))
+do
+    $DAEMON unsafe-reset-all  --home $DAEMON_HOME-$a
+
+    echo "-- Executed $DAEMON unsafe-reset-all  --home $DAEMON_HOME-$a --"
+done
+
+echo
+
+echo "---------- Stopping systemd service files --------"
+
+for (( a=1; a<=$NODES; a++ ))
+do
+    sudo -S systemctl stop $DAEMON-${a}.service
+
+    echo "-- Executed sudo -S systemctl stop $DAEMON-${a}.service --"
+done
+
+echo
+
+echo "---------- Disabling systemd process files --------"
+
+for (( a=1; a<=$NODES; a++ ))
+do
+   sudo -S systemctl disable $DAEMON-${a}.service
+
+   echo "-- Executed sudo -S systemctl disable $DAEMON-${a}.service --"
+done
+
+echo
